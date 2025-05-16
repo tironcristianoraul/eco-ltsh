7ab8e936848408beae9d363ceb23dd978c6f8ba5
tironcristianoraul <raultiron938@gmail.com>
2025-05-16 19:10:10 +0300

    sgia

--- src/views/sign-in/index.tsx
+++ src/views/sign-in/index.tsx
@@ -1,10 +1,10 @@
-import { Box, Button, Input } from '@mui/material';
-import { useEffect, useState } from 'react';
-import useAuth from '../../hooks/use-auth';
-import { useNavigate } from 'react-router';
+import { Box, Button, Input } from "@mui/material";
+import { useEffect, useState } from "react";
+import useAuth from "../../hooks/use-auth";
+import { useNavigate } from "react-router";
 
-import { SignInBox } from './index.styled.ts';
-import forestImg from '../../assets/forest3.jpg';
+import { SignInBox } from "./index.styled.ts";
+import forestImg from "../../assets/forest3.jpg";
 
 export interface SignInInputs {
   email: string;
@@ -12,46 +12,53 @@ export interface SignInInputs {
 }
 
 const SignIn = () => {
-
   const navigate = useNavigate();
 
   const { login, isLoggedIn } = useAuth();
 
-  const [email, setEmail] = useState<string>('')
-  const [password, setPassword] = useState<string>('')
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
   const handleSubmit = () => {
     login({
       email: email,
-      password: password
+      password: password,
     });
   };
 
   useEffect(() => {
-    if (isLoggedIn)
-      navigate('/');
-  }, [isLoggedIn, navigate])
+    if (isLoggedIn) navigate("/");
+  }, [isLoggedIn, navigate]);
 
   return (
-    <Box sx={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100vh',
-      width: '100vw',
-      backgroundImage: `url(${forestImg})`,
-      backroundSize : '100vh',
-      backgroundPosition: 'center',
-      backgroundRepeat: 'no-repeat',
-      }}>
-        <SignInBox>
-        <Input type='email' name='email' onChange={(e) => setEmail(() => e.target.value)} value={email} />
-        <Input type='password' name='password' onChange={(e) => setPassword(() => e.target.value)} value={password} />
-        <Button onClick={handleSubmit} >Logare</Button>
-        </SignInBox>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        width: "100vw",
+        backgroundImage: `url(${forestImg})`,
+        backgroundSize: "cover",
+      }}
+    >
+      <SignInBox>
+        <Input
+          type="email"
+          name="email"
+          onChange={(e) => setEmail(() => e.target.value)}
+          value={email}
+        />
+        <Input
+          type="password"
+          name="password"
+          onChange={(e) => setPassword(() => e.target.value)}
+          value={password}
+        />
+        <Button onClick={handleSubmit}>Logare</Button>
+      </SignInBox>
     </Box>
- 
-  )
+  );
 };
 
 export default SignIn;
