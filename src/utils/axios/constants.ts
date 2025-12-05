12fff5adb3c7e47843833d2808cb7759ab5731b2
tironcristianoraul <raultiron938@gmail.com>
2025-12-05 10:10:01 +0200

    push bengos

--- src/utils/axios/constants.ts
+++ src/utils/axios/constants.ts
@@ -2,10 +2,10 @@ const { VITE_SCENARIO } = import.meta.env;
 
 const URL =
   VITE_SCENARIO === "local_port"
-    ? "http://95.76.109.39:2186"
+    ? "http://eco-ltsh-be.vercel.app"
     : VITE_SCENARIO === "local"
     ? "http://127.0.0.1:2186"
-    : "http://95.76.109.39:2186"; // for now;
+    : "http://eco-ltsh-be.vercel.app"; // for now;
 
 const baseUrl: string = `${URL}/eco-ltsh/api/admin`;
 
